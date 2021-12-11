300c96249531e5790d91263f537138809711d79b
Eleonora <eleonora1weber@gmail.com>
2021-12-11 14:14:18 +0100

    added some ts code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement, ReactNode } from 'react';
 import './App.css';
 
+// Conventinal props
+function Heading({ title }: { title: string; }) {
+  return <h1>{title}</h1>
+}
+function HeadingWithContent({ children }: { children: ReactNode }): ReactElement {
+  return <h1>{children}</h1>;
+}
+
+// defaultProps
+const defaultContainerProps = {
+  heading: <strong>My Heading</strong>
+};
+type ContainerProps = { children: ReactNode } & typeof defaultContainerProps
+function Container({ heading, children }: ContainerProps): ReactElement {
+  return <div><h1>{heading}</h1>{children}</div>
+}
+Container.defaultProps = defaultContainerProps;
+
+// Functinal props
+function TextWithNumber({children}: {children: (num: number) => ReactNode
+}) {
+  const [state, stateSet] = React.useState<number>(1);
+  return (
+    <div>
+      <div>
+        {children(state)}
+      </div>
+      <div>
+        <button onClick={() => stateSet(state + 1)}>Add</button>
+      </div>
+    </div>
+  )
+}
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="Hello"></Heading>
+      <HeadingWithContent><strong>Hi!</strong></HeadingWithContent>
+      <Container>Foo</Container>
+      <TextWithNumber>{(num: number) => <div> Today's number is {num} </div> }</TextWithNumber>
     </div>
-  );
-}
+  )
+};
 
 export default App;
